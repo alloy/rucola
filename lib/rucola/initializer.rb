49f7e8b373f31c8c1978cef1eba43bfaf6194347
Eloy Duran <eloy.de.enige@gmail.com>
2007-10-20 18:49:53 +0000

    2007-10-20 Eloy Duran <e.duran@superalloy.nl>
    * lib/rucola/initializer.rb:
    Fixed a bug where hidden files in the plugins dir where also required.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rucola/trunk@8 b89190f6-c990-47a9-a149-8f829472b86c

--- lib/rucola/initializer.rb
+++ lib/rucola/initializer.rb
@@ -204,6 +204,7 @@ module Rucola
     @@required_plugins = [] # TODO: isn't used yet
     if RUBYCOCOA_PLUGINS_ROOT.exist?
       RUBYCOCOA_PLUGINS_ROOT.children.each do |plugin|
+        next if plugin.basename.to_s[0,1] == '.'
         @@required_plugins.push plugin
         require plugin + 'init.rb'
       end
