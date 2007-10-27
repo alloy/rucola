719a2929b5852354ee5f262df22261fb82912dc2
Justin Palmer <encytemedia@gmail.com>
2007-10-27 23:13:19 +0000

    Add controller generator and simiple usage descriptions for both generators
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rucola/trunk@26 b89190f6-c990-47a9-a149-8f829472b86c

---
+++ rucola_generators/controller/controller_generator.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'rucola/rucola_support/core_ext/string'
+
+class ControllerGenerator < RubiGen::Base
+  
+  default_options :author => nil
+  
+  attr_reader :name
+  
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @name = args.shift
+    extract_options
+  end
+
+  def manifest
+    record do |m|
+      # Ensure appropriate folder(s) exists
+      controller_dir = 'app/controllers'
+      test_dir       = 'test/controllers'
+      
+      m.directory controller_dir
+      m.directory test_dir
+
+      # Create stubs
+      m.template 'controller_template.rb.erb', "#{controller_dir}/#{@name.snake_case}_controller.rb"
+      m.template 'test_controller_template.rb.erb', "#{test_dir}/test_#{@name.snake_case}_controller.rb"
+    end
+  end
+
+  protected
+    def banner
+      <<-EOS
+Creates a ...
+
+USAGE: #{$0} #{spec.name} name"
+EOS
+    end
+
+    def add_options!(opts)
+      # opts.separator ''
+      # opts.separator 'Options:'
+      # For each option below, place the default
+      # at the top of the file next to "default_options"
+      # opts.on("-a", "--author=\"Your Name\"", String,
+      #         "Some comment about this option",
+      #         "Default: none") { |options[:author]| }
+      # opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
+    end
+    
+    def extract_options
+      # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
+      # Templates can access these value via the attr_reader-generated methods, but not the
+      # raw instance variable value.
+      # @author = options[:author]
+    end
+end
