2adbe15cd82ae4226f6c0a71f6bcb38269d45c34
Eloy Duran <eloy.de.enige@gmail.com>
2008-01-11 14:45:47 +0000

    Fixed some minor errors in the Reloader docs.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rucola/trunk@153 b89190f6-c990-47a9-a149-8f829472b86c

--- lib/rucola/reloader.rb
+++ lib/rucola/reloader.rb
@@ -5,7 +5,7 @@ module Rucola
   # a class if it notices that a file has been changed.
   module Reloader
     class << self
-      # # Start watching app/controllers for a file modification and reload that class.
+      # Start watching app/controllers for a file modification and reload that class.
       def start!
         # Start watching the app/controllers dir.
         Rucola::FSEvents.start_watching(Rucola::RCApp.controllers_path) do |events|
@@ -23,7 +23,7 @@ module Rucola
         OSX.NSLog "- Undefining instance methods: #{i_methods.inspect}"
         i_methods.each { |mname| klass.send(:undef_method, mname) }
       
-        # undefine class methdos
+        # undefine class methods
         c_methods = klass.original_class_methods
         OSX.NSLog "- Undefining class methods: #{c_methods.inspect}"
         c_methods.each { |mname| klass.metaclass.send(:undef_method, mname) }
