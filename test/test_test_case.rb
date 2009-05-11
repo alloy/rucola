e736d0d9aefcd6bd426ef4f88c71a46077c10bee
Eloy Duran <eloy.de.enige@gmail.com>
2009-05-11 17:53:30 +0200

    Cleaner test case

--- test/test_test_case.rb
+++ test/test_test_case.rb
@@ -9,36 +9,65 @@ class ATestCase
   tests ATestController
 end
 
-describe "Rucola::TestCase" do
+class ATestCaseWithCustomSetupAndTeardown
+  extend Rucola::TestCase
+  tests ATestController
+  
+  def setup
+    :aliased
+  end
+  
+  def teardown
+    :aliased
+  end
+end
+
+describe "The Rucola::TestCase class" do
   before do
     @test_case = ATestCase.new
   end
   
+  it "should register the class to test" do
+    ATestCase.instance_variable_get(:@class_to_be_tested).should.be ATestController
+  end
+  
+  it "should include the Rucola::TestCase::InstanceMethods module into the TestCase" do
+    ATestCase.ancestors.should.include Rucola::TestCase::InstanceMethods
+  end
+  
   it "should define a public setup method and a private alias" do
-    ATestCase.instance_methods.should.include 'setup'
+    ATestCase.public_instance_methods.should.include 'setup'
     ATestCase.private_instance_methods.should.include 'rucola_test_case_setup'
   end
   
   it "should define a public teardown method and a private alias" do
-    ATestCase.instance_methods.should.include 'teardown'
+    ATestCase.public_instance_methods.should.include 'teardown'
     ATestCase.private_instance_methods.should.include 'rucola_test_case_teardown'
   end
   
-  it "should alias a new user defined #setup method to #after_setup and re-alias Rucola's setup to #setup" do
-    @test_case.should.not.respond_to :after_setup
-    ATestCase.class_eval { def setup; :aliased end }
-    @test_case.after_setup.should.be :aliased
+  it "should alias a new user defined #setup method to #after_setup" do
+    ATestCase.public_instance_methods.should.not.include 'after_setup'
     
-    @test_case.expects(:after_setup)
-    @test_case.setup
+    test_case = ATestCaseWithCustomSetupAndTeardown.new
+    test_case.after_setup.should.be :aliased
   end
   
-  it "should alias a new user defined #teardown method to #after_teardown and re-alias Rucola's teardown to #teardown" do
-    @test_case.should.not.respond_to :after_teardown
-    ATestCase.class_eval { def teardown; :aliased end }
-    @test_case.after_teardown.should.be :aliased
+  it "should re-alias Rucola's #rucola_test_case_setup to #setup" do
+    test_case = ATestCaseWithCustomSetupAndTeardown.new
+    test_case.expects(:after_setup)
+    test_case.setup
+  end
+  
+  it "should alias a new user defined #teardown method to #after_teardown" do
+    ATestCase.public_instance_methods.should.not.include 'after_teardown'
     
-    @test_case.expects(:after_teardown)
-    @test_case.teardown
+    test_case = ATestCaseWithCustomSetupAndTeardown.new
+    test_case.after_teardown.should.be :aliased
+  end
+  
+  it "should re-alias Rucola's #rucola_test_case_teardown to #teardown" do
+    test_case = ATestCaseWithCustomSetupAndTeardown.new
+    test_case.expects(:after_teardown)
+    test_case.teardown
   end
 end
