631b10346debab41299c03d342162cd30956966a
Eloy Duran <eloy.de.enige@gmail.com>
2009-11-30 23:19:19 +0100

    Imported a MacRuby application as created by XCode.

---
+++ test-templates/MacRubyApplication/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacRubyApplication
+//
+//  Created by Eloy Duran on 30-11-09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
---
+++ test-templates/MacRubyApplication/rb_main.rb
@@ -0,0 +1,22 @@
+#
+# rb_main.rb
+# MacRubyApplication
+#
+# Created by Eloy Duran on 30-11-09.
+# Copyright __MyCompanyName__ 2009. All rights reserved.
+#
+
+# Loading the Cocoa framework. If you need to load more frameworks, you can
+# do that here too.
+framework 'Cocoa'
+
+# Loading all the Ruby project files.
+dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+Dir.glob(dir_path + '/*.{rb,rbo}').map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
+  if path != File.basename(__FILE__)
+    require(path)
+  end
+end
+
+# Starting the Cocoa main loop.
+NSApplicationMain(0, nil)
