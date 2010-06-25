8d9c3052d992f57263d9fecdc91f9683d6504833
Eloy Duran <eloy.de.enige@gmail.com>
2010-06-25 16:12:09 +0200

    Stash ongoing prototype hacking

--- lib/rucola/generators/rucola/app/app_generator.rb
+++ lib/rucola/generators/rucola/app/app_generator.rb
@@ -2,40 +2,96 @@ require 'rucola/generators/base'
 
 module Rucola
   module Generators
-    class AppBase < Base
-      argument :app_path, :type => :string
-      
-      def self.source_root
-        '/Library/Application Support/Developer/Shared/Xcode/Project Templates/Application'
+    module App
+      class Type < Thor::Group
+        argument :project_type, :type => :string
+        
+        def invoke_app_generator
+          type = ARGV.shift
+          generator_name = "#{project_type.camelize}Generator"
+          invoke Rucola::Generators::App.const_get(generator_name)
+        end
+        
+        def self.banner
+          "rucola new #{arguments.map(&:usage).join(' ')}"
+        end
+        
+        def self.desc
+          "PROJECT_TYPE can be one of: #{Base.generators.join(', ')}"
+        end
       end
       
-      def create_root
-        self.destination_root = File.expand_path(app_path, destination_root)
-        empty_directory '.'
+      class Base < Rucola::Generators::Base
+        argument :app_path, :type => :string
+        
+        attr_accessor :app_name
+        
+        # Keep a list of generators to display to the user in the Type banner
+        def self.inherited(generator)
+          (@generators ||= []) << generator.generator_name
+        end
+        def self.generators; @generators; end
+        
+        def self.source_root
+          '/Library/Application Support/Developer/Shared/Xcode/Project Templates/Application'
+        end
+        
+        def self.xcodeproj_template
+          Dir.glob(File.join(source_root, '*.xcodeproj')).first
+        end
+        
+        def self.xcodeproj_template_info
+          path = File.join(xcodeproj_template, 'TemplateInfo.plist')
+          NSDictionary.dictionaryWithContentsOfFile(path)
+        end
+        
+        def self.banner
+          "rucola new #{arguments.map(&:usage).join(' ')} [options]"
+        end
+        
+        def self.desc
+          xcodeproj_template_info['Description']
+        end
+        
+        def initialize(*args)
+          super
+          self.app_name = File.basename(app_path).camelize
+          self.destination_root = File.expand_path(app_path, destination_root)
+        end
+        
+        def create_root
+          empty_directory '.'
+        end
+        
+        def create_xcodeproj
+          xcodeproj = "#{app_name}.xcodeproj"
+          empty_directory xcodeproj
+          template "MacRubyApp.xcodeproj/project.pbxproj", File.join(xcodeproj, "project.pbxproj")
+        end
       end
-    end
-    
-    class AppGenerator < AppBase
-      def self.source_root
-        @source_root ||= File.join(super, 'MacRuby Application')
+      
+      class AppGenerator < Base
+        def self.source_root
+          @source_root ||= File.join(super, 'MacRuby Application')
+        end
       end
-    end
-    
-    class CoreDataAppGenerator < AppBase
-      def self.source_root
-        @source_root ||= File.join(super, 'MacRuby Core Data Application')
+      
+      class CoreDataAppGenerator < Base
+        def self.source_root
+          @source_root ||= File.join(super, 'MacRuby Core Data Application')
+        end
       end
-    end
-    
-    class DocumentAppGenerator < AppBase
-      def self.source_root
-        @source_root ||= File.join(super, 'MacRuby Document-based Application')
+      
+      class DocumentAppGenerator < Base
+        def self.source_root
+          @source_root ||= File.join(super, 'MacRuby Document-based Application')
+        end
       end
-    end
-    
-    class PrefPaneGenerator < AppBase
-      def self.source_root
-        @source_root ||= File.join(super, 'MacRuby Preference Pane')
+      
+      class PrefPaneGenerator < Base
+        def self.source_root
+          @source_root ||= File.join(super, 'MacRuby Preference Pane')
+        end
       end
     end
   end
