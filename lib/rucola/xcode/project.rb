770c4c86b932d35f54de0baa17bd4ff02327be8b
Eloy Duran <eloy.de.enige@gmail.com>
2010-06-28 15:59:29 +0200

    Add a first version of Rucola::XCode::Project which can remove file and group objects.

--- lib/rucola/xcode/project.rb
+++ lib/rucola/xcode/project.rb
@@ -1,6 +1,52 @@
 module Rucola
   module XCode
     class Project
+      def initialize(path)
+        @path = path
+      end
+      
+      def data
+        @data ||= Hash.dictionaryWithContentsOfFile(File.join(@path, 'project.pbxproj'))
+      end
+      
+      def objects
+        data['objects']
+      end
+      
+      def file_objects
+        objects.select { |_, object| object['isa'] == 'PBXFileReference' }
+      end
+      
+      def group_objects
+        objects.select { |_, object| object['isa'] == 'PBXGroup' }
+      end
+      
+      def file_object(filename)
+        file_objects.find { |_, object| object['path'] == filename }
+      end
+      
+      def group_object(group_name)
+        group_objects.find { |_, object| object['path'] == group_name }
+      end
+      
+      def group_objects_by_child_uuid(child_uuid)
+        group_objects.select { |_, object| object['children'].include? child_uuid }
+      end
+      
+      def remove_file(filename)
+        uuid, _ = file_object(filename)
+        if uuid
+          objects.delete(uuid)
+          group_objects_by_child_uuid(uuid).each do |_, group|
+            group['children'].delete(uuid)
+          end
+        end
+      end
+      
+      def remove_group(group_name)
+        uuid, _ = group_object(group_name)
+        objects.delete(uuid) if uuid
+      end
     end
   end
 end
--- spec/unit/xcode/project_spec.rb
+++ spec/unit/xcode/project_spec.rb
@@ -1,3 +1,68 @@
 # encoding: UTF-8
 require File.expand_path("../../../spec_helper", __FILE__)
 require 'rucola/xcode/project'
+
+describe "Rucola::XCode::Project" do
+  before do
+    @path = fixture('expected/Übercøøl/Übercøøl.xcodeproj')
+    @project = Rucola::XCode::Project.new(@path)
+  end
+  
+  def data
+    @data ||= Hash.dictionaryWithContentsOfFile(File.join(@path, 'project.pbxproj'))
+  end
+  
+  it "loads and returns the data at the given project path" do
+    @project.data.should == data
+  end
+  
+  it "returns the `objects'" do
+    @project.objects.should == data['objects']
+  end
+  
+  it "returns just the file objects" do
+    @project.file_objects.should.not.be.empty
+    @project.file_objects.each do |uuid, object|
+      object['isa'].should == 'PBXFileReference'
+    end
+  end
+  
+  it "returns a file object by filename" do
+    uuid, object = @project.file_object('run_suite.rb')
+    uuid.should == '17D55CD81076A1A2008207BD'
+    object['path'].should == 'run_suite.rb'
+  end
+  
+  it "returns just the group objects" do
+    @project.group_objects.should.not.be.empty
+    @project.group_objects.each do |uuid, object|
+      object['isa'].should == 'PBXGroup'
+    end
+  end
+  
+  it "returns groups by a matching child object" do
+    child_uuid = '17D55CD81076A1A2008207BD'
+    groups = @project.group_objects_by_child_uuid(child_uuid)
+    groups.length.should == 1
+    uuid, object = groups.first
+    uuid.should == '172754AE1075979200D0347B'
+    object['children'].should.include child_uuid
+  end
+  
+  it "returns a group object by group name" do
+    uuid, object = @project.group_object('Tests')
+    uuid.should == '172754AE1075979200D0347B'
+    object['path'].should == 'Tests'
+  end
+  
+  it "removes a file object by filename and from the groups it belongs to" do
+    @project.remove_file('run_suite.rb')
+    @project.file_object('run_suite.rb').should == nil
+    @project.group_objects_by_child_uuid('17D55CD81076A1A2008207BD').should.be.empty
+  end
+  
+  it "removes a group by a group name" do
+    @project.remove_group('Tests')
+    @project.group_objects_by_child_uuid('17D55CD81076A1A2008207BD').should.be.empty
+  end
+end
